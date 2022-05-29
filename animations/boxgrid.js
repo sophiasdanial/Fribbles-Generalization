f13d8df3c8620541fa6a4ad3f9e70d06d9399d00
sophiasdanial <s.s.x.danial@gmail.com>
2022-05-30 00:00:41 +0800

    convert boxgrid to js

---
+++ animations/boxgrid.js
@@ -0,0 +1,80 @@
+//declaring variables
+  var counter = 0;
+  var answersheet = new Array;
+  var opened = new Array;
+  var correctboxes = 6;
+  var wrongboxes = 3;
+  var correctcounter = 0;
+  var fribpics = new Array;
+  fribpics = [
+    '1211.jpg',
+    '1131.jpg',
+    '1321.jpg',
+    '1121.jpg',
+    '1231.jpg',
+    '1311.jpg'
+  ];
+
+
+  //generating answersheet
+  while (correctboxes > 2){
+    answersheet.push(true);
+    correctboxes -= 1;
+  }
+  while (wrongboxes > 0){
+    answersheet.push(false);
+    wrongboxes -= 1;
+  }
+
+  //box press - check if box is already opened and if 9 boxes already opened
+  function change(buttonid) {
+	  if (opened.includes(buttonid) || counter >= 9){}
+    else {
+    opened.push(buttonid);
+      addCount(buttonid);
+      
+    }
+  }
+
+  function addCount(buttonid) {
+    counter += 1;
+    var correct = false;
+    if(counter == 1){ //opening box 1
+      correct = true;
+    }
+    else if(counter < 9){ //opening boxes 2-8
+      var determiner = Math.random()
+      if(determiner > 0.5){
+        correct = answersheet.shift();
+      }
+      else{
+        correct = answersheet.pop();
+      }
+    }
+    else if (counter == 9){ //opening box 9
+      correct = true;
+      finish();
+    }
+    else { //attempting to open box 10+
+    }
+    if (correct){
+      addpicture();
+      correctcounter += 1;
+      document.getElementById("counter").innerHTML = "Example counter: (" + (3 + correctcounter) + ")";
+    }
+    if (correct) {
+      document.getElementById(buttonid).src = "https://sophiasdanial.github.io/Fribbles-Generalization/images/tick.png";
+    }
+    else {
+      document.getElementById(buttonid).src = "https://sophiasdanial.github.io/Fribbles-Generalization/images/cross.png";
+    }
+  }
+
+  //correct box found
+  function addpicture(){
+    //whatever you want to do when they open a 'correct' box
+    var fribid = "frib" + (correctcounter + 1);
+    var friblink = 'https://sophiasdanial.github.io/Fribbles-Generalization/images/Fc1-training/Fc1_' + fribpics[correctcounter];
+    document.getElementById(fribid).src = friblink;
+
+    }
