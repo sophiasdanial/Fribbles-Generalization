7e985c4f0bb89e943152290eab18ff66e9498c8c
sophiasdanial <s.s.x.danial@gmail.com>
2022-06-24 03:05:57 +0800

    Update UCgrid.js

--- animations/UCgrid.js
+++ animations/UCgrid.js
@@ -72,6 +72,27 @@
     }
     document.getElementById("ncounter").innerHTML = (11 - counter);
   }
+///// timer 
+function startTimer() {
+  var presentTime = document.getElementById("timer").innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){
+    return
+  }
+  document.getElementById("timer").innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
 
 //correct box found
 function addpicture(){
@@ -83,4 +104,5 @@ function addpicture(){
 
 function finish(){
 //whatever you want to do when they open 11 boxes
+    
 }
