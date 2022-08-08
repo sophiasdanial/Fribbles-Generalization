b14730e4b0d135575aa24558ca8d2eda4da3adaa
sophiasdanial <s.s.x.danial@gmail.com>
2022-08-08 19:32:14 +0800

    update UC.js

--- animations/UCgrid.js
+++ animations/UCgrid.js
@@ -51,8 +51,6 @@
     else if (counter == 8){ //opening box 8
       correct = true;
       finish();
-      document.getElementById("buttonapp").innerHTML = "...Please wait for the 'next' button to appear";
-      document.getElementById("timer").style = "color:red";
     }
     else { //attempting to open box 10+
     }
@@ -69,27 +67,6 @@
     }
     document.getElementById("ncounter").innerHTML = (8 - counter);
   }
-///// timer 
-function startTimer() {
-  var presentTime = document.getElementById("timer").innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  if(m<0){
-    return
-  }
-  document.getElementById("timer").innerHTML =
-    m + ":" + s;
-  console.log(m)
-  setTimeout(startTimer, 1000);
-}
-
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec < 0) {sec = "59"};
-  return sec;
-}
 
 //correct box found
 function addpicture(){
