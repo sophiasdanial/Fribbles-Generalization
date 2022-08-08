250663d8f1ff3c135d41b8f88ce0031ac8df9e88
sophiasdanial <s.s.x.danial@gmail.com>
2022-08-08 17:55:07 +0800

    animation

--- animations/UCgrid.js
+++ animations/UCgrid.js
@@ -2,17 +2,14 @@
   var counter = 0;
   var answersheet = new Array;
   var opened = new Array;
-  var correctboxes = 6;
+  var correctboxes = 3;
   var wrongboxes = 5;
   var correctcounter = 0;
   var fribpics = new Array;
   fribpics = [
-    '3132.png',
-    '1212.png',
-    '1313.png',
-    '2123.png',
-    '2221.png',
-    '3331.png'
+    '1222.png',
+    '2133.png',
+    '3311.png'
   ];
 
 
@@ -29,7 +26,7 @@
 
   //box press - check if box is already opened and if 9 boxes already opened
   function change(buttonid) {
-	  if (opened.includes(buttonid) || counter >= 11){}
+	  if (opened.includes(buttonid) || counter >= 8){}
     else {
     opened.push(buttonid);
       addCount(buttonid);
@@ -42,7 +39,7 @@
     if(counter == 1){ //opening box 1
       correct = true;
     }
-    else if(counter < 11){ //opening boxes 2-10
+    else if(counter < 8){ //opening boxes 2-7
       var determiner = Math.random()
       if(determiner > 0.5){
         correct = answersheet.shift();
@@ -51,7 +48,7 @@
         correct = answersheet.pop();
       }
     }
-    else if (counter == 11){ //opening box 11
+    else if (counter == 8){ //opening box 8
       correct = true;
       finish();
       document.getElementById("buttonapp").innerHTML = "...Please wait for the 'next' button to appear";
@@ -70,7 +67,7 @@
     else {
       document.getElementById(buttonid).src = "https://sophiasdanial.github.io/Fribbles-Generalization/images/whoops.png";
     }
-    document.getElementById("ncounter").innerHTML = (11 - counter);
+    document.getElementById("ncounter").innerHTML = (8 - counter);
   }
 ///// timer 
 function startTimer() {
